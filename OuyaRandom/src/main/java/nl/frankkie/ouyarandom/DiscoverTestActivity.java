3154678b41173931b34932fa1afd715c95be17e1
frankkienl <frankkie12345@gmail.com>
2013-07-05 20:36:29 +0200

    Test to check if we can turn of the device (like the stock launcher does)

--- OuyaRandom/src/main/java/nl/frankkie/ouyarandom/DiscoverTestActivity.java
+++ OuyaRandom/src/main/java/nl/frankkie/ouyarandom/DiscoverTestActivity.java
@@ -26,6 +26,8 @@ public class DiscoverTestActivity extends Activity {
         Button btn1 = (Button) findViewById(R.id.discover1);
         Button btn2 = (Button) findViewById(R.id.discover2);
         Button btn3 = (Button) findViewById(R.id.discover3);
+        Button btn4 = (Button) findViewById(R.id.discover4);
+        Button btn5 = (Button) findViewById(R.id.discover5);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +62,24 @@ public class DiscoverTestActivity extends Activity {
                 task.execute();
             }
         });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent localIntent = new Intent("tv.ouya.console.action.TURN_OFF");
+                try {
+                    sendBroadcast(localIntent);
+                } catch (Exception e) {
+                    ShowException.showException(e, DiscoverTestActivity.this);
+                }
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               TurnOffAsyncTask task = new TurnOffAsyncTask();
+                task.execute();
+            }
+        });
     }
 
 
@@ -74,6 +94,25 @@ public class DiscoverTestActivity extends Activity {
                 //String suVersionInternal = Shell.SU.version(true);
                 List<String> suResult = Shell.SU.run(new String[]{
                         "am start --user 0 -n tv.ouya.console/tv.ouya.console.launcher.store.adapter.DiscoverActivity"
+                });
+
+            }
+            return null;
+        }
+
+    }
+
+    private class TurnOffAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            // Let's do some SU stuff
+            boolean suAvailable = Shell.SU.available();
+            if (suAvailable) {
+                //String suVersion = Shell.SU.version(false);
+                //String suVersionInternal = Shell.SU.version(true);
+                List<String> suResult = Shell.SU.run(new String[]{
+                        "am broadcast --user 0 -a tv.ouya.console.action.TURN_OFF"
                 });
 
             }
