eeb4b17cffe7871644cb785ca3e81e3926b95016
frankkienl <frankkie12345@gmail.com>
2013-07-08 21:41:50 +0200

    Added new Discover-test

--- OuyaRandom/src/main/java/nl/frankkie/ouyarandom/DiscoverTestActivity.java
+++ OuyaRandom/src/main/java/nl/frankkie/ouyarandom/DiscoverTestActivity.java
@@ -4,16 +4,23 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import eu.chainfire.libsuperuser.Shell;
 
+import javax.net.ssl.HandshakeCompletedListener;
+import java.io.*;
 import java.util.List;
 
 /**
  * @author FrankkieNL
  */
 public class DiscoverTestActivity extends Activity {
+
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +30,14 @@ public class DiscoverTestActivity extends Activity {
 
     protected void initUI() {
         setContentView(R.layout.discover_test);
-        Button btn1 = (Button) findViewById(R.id.discover1);
-        Button btn2 = (Button) findViewById(R.id.discover2);
-        Button btn3 = (Button) findViewById(R.id.discover3);
-        Button btn4 = (Button) findViewById(R.id.discover4);
-        Button btn5 = (Button) findViewById(R.id.discover5);
+        Button btnNormal = (Button) findViewById(R.id.discover_discover_normal);
+        Button btnOld = (Button) findViewById(R.id.discover_discover_old);
+        Button btnRoot1 = (Button) findViewById(R.id.discover_discover_root1);
+        Button btnRoot2 = (Button) findViewById(R.id.discover_discover_root2);
+        Button btnOff = (Button) findViewById(R.id.discover_off);
+        Button btnOffRoot1 = (Button) findViewById(R.id.discover_off_root);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        btnNormal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent();
@@ -42,7 +50,7 @@ public class DiscoverTestActivity extends Activity {
                 }
             }
         });
-        btn2.setOnClickListener(new View.OnClickListener() {
+        btnOld.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent();
@@ -55,14 +63,14 @@ public class DiscoverTestActivity extends Activity {
                 }
             }
         });
-        btn3.setOnClickListener(new View.OnClickListener() {
+        btnRoot1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 StartDiscoverRootAsyncTask task = new StartDiscoverRootAsyncTask();
                 task.execute();
             }
         });
-        btn4.setOnClickListener(new View.OnClickListener() {
+        btnOff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent localIntent = new Intent("tv.ouya.console.action.TURN_OFF");
@@ -73,10 +81,18 @@ public class DiscoverTestActivity extends Activity {
                 }
             }
         });
-        btn5.setOnClickListener(new View.OnClickListener() {
+        btnOffRoot1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               TurnOffAsyncTask task = new TurnOffAsyncTask();
+                TurnOffAsyncTask task = new TurnOffAsyncTask();
+                task.execute();
+            }
+        });
+
+        btnRoot2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StartDiscoverRoot2AsyncTask task = new StartDiscoverRoot2AsyncTask();
                 task.execute();
             }
         });
@@ -95,7 +111,17 @@ public class DiscoverTestActivity extends Activity {
                 List<String> suResult = Shell.SU.run(new String[]{
                         "am start --user 0 -n tv.ouya.console/tv.ouya.console.launcher.store.adapter.DiscoverActivity"
                 });
-
+                String temp = "Result from SU:\n";
+                for (String s : suResult){
+                    temp+=s + "\n";
+                }
+                final String toastLine = temp;
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(DiscoverTestActivity.this,toastLine, Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
             return null;
         }
@@ -119,6 +145,60 @@ public class DiscoverTestActivity extends Activity {
             return null;
         }
 
+    }
+
+    private class StartDiscoverRoot2AsyncTask extends AsyncTask<Void,Void,Void>{
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Shell.Builder b = new Shell.Builder();
+            Shell.Interactive interactive =  b.open();
+            interactive.addCommand("su");
+            interactive.waitForIdle();
+            interactive.addCommand("am start --user 0 -n tv.ouya.console/tv.ouya.console.launcher.store.adapter.DiscoverActivity");
+            interactive.waitForIdle();
+            interactive.close();
+            return null;
+        }
+    }
+
+    private class ProcessInThread extends Thread {
+        InputStream in;
+
+        ProcessInThread(InputStream inputStream) {
+            this.in = inputStream;
+        }
+
+
+        @Override
+        public void run() {
+            InputStreamReader reader = new InputStreamReader(in);
+            BufferedReader bin = new BufferedReader(reader);
+            String line = null;
+            boolean go = true;
+            try {
+                while (go) {
+                    line = bin.readLine();
+                    Log.i("DiscoverTest", line);
+                    final String toastLine = line;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(DiscoverTestActivity.this,toastLine, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    if (line == null) {
+                        break;
+                    }
+                }
+            } catch (final IOException ioe) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        ShowException.showException(ioe, DiscoverTestActivity.this);
+                    }
+                });
+            }
+        }
     }
 
 }
